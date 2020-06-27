547c7eb01a83a0c108219070f6950ba4aa01ac6f
Vasiliy <kkolyasev1228@gmail.com>
2020-06-27 14:26:49 +0300

    view comment

--- controllers/cat.php
+++ controllers/cat.php
@@ -1,7 +1,6 @@
 <?php
 $id_cat = URL_PARAMS[1];
 $statesCat = states_cat_login_select($id_cat);
-var_dump($statesCat);
 
 $pageTitle = 'states | ' . $statesCat[0]['cat_title'];
 $pageContent = template('v_states', ['states' => $statesCat]);
--- views/v_state.php
+++ views/v_state.php
@@ -9,12 +9,42 @@
 				<p class="card-text">Autor: <?=$state['login']?></p>
 				<hr>
 				<?if($user !== null):?>
-				<form method="post">
-					<input type="text" name="login" required placeholder="login"><br>
-					<textarea name="comment" id="" cols="30" rows="10" placeholder="comment"></textarea><br>
-					<input type="submit"><br>
-				</form>
+                    <form method="post">
+                        <input type="text" name="login" required placeholder="login"><br>
+                        <textarea name="comment" id="" cols="30" rows="10" placeholder="comment"></textarea><br>
+                        <input type="submit"><br>
+                    </form>
+                    <hr>
 				<?endif;?>
 			</div>
+            <div class="card-footer">
+                <div class="comments">
+                    <h3 class="title-comments">Комментарии</h3>
+                    <ul class="media-list">
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#">
+                                    <img class="media-object img-rounded" src="avatar1.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <div class="media-heading">
+                                    <div class="author">Дима</div>
+                                    <div class="metadata">
+                                        <span class="date">16 ноября 2015, 13:43</span>
+                                    </div>
+                                </div>
+                                <div class="media-text text-justify">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Asperiores dicta doloremque et explicabo, facere minus nisi
+                                    nostrum numquam ratione tempora! Aspernatur atque dignissimos
+                                    exercitationem expedita fugiat fugit hic laborum maxime molestiae
+                                    odit optio placeat provident, quod sunt, tempora totam voluptatum.
+                                    Consequatur culpa fuga nostrum quaerat quidem quod rerum, tempore vero.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+            </div>
 		</div>
 </div>
